1b10243b255ab10a5e0def31161c9168146dfe8a
Vitória Vitoria <vitoriasilva98@hotmail.com>
2024-12-04 21:10:33 -0300

    Desafio Final - Ajuste no serviço "Usuário" para conexão com "ConfigServer"

--- desafio_final/memelandia-usuario/src/main/java/br/com/ebac/memelandia_usuario/MemelandiaUsuarioApplication.java
+++ desafio_final/memelandia-usuario/src/main/java/br/com/ebac/memelandia_usuario/MemelandiaUsuarioApplication.java
@@ -1,9 +1,16 @@
 package br.com.ebac.memelandia_usuario;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+//@RefreshScope
+//@EnableFeignClients
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class MemelandiaUsuarioApplication {
 
 	public static void main(String[] args) {
--- desafio_final/memelandia-usuario/src/main/java/br/com/ebac/memelandia_usuario/repositories/IUsuarioRepository.java
+++ desafio_final/memelandia-usuario/src/main/java/br/com/ebac/memelandia_usuario/repositories/IUsuarioRepository.java
@@ -3,9 +3,11 @@ package br.com.ebac.memelandia_usuario.repositories;
 import br.com.ebac.memelandia_usuario.entities.Usuario;
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface IUsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByEmail(@NotBlank(message = "O email do usuário é obrigatório") String email);
 }
